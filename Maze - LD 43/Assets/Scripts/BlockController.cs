6e8dc086aea8a533c52bd066e08e2f8c73a90168
thiagocgp <thiagocgp@gmail.com>
2018-12-01 12:52:45 -0200

    Game

---
+++ Maze - LD 43/Assets/Scripts/BlockController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour {
+
+    public bool isSolidBlock = true;
+    public bool hasPlayer = false;
+    public bool fall;
+    public int[] blockPosion;
+
+    // Use this for initialization
+    void Start ()
+    {
+        blockPosion = new int[2];
+        getBlockPosition();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        checkPlayer();
+        checkPositionToDestroy();
+    }
+
+    void getBlockPosition()
+    {
+        string nome = gameObject.name;
+        int lin = int.Parse(nome.Substring(nome.Length - 2, 1));
+        int col = int.Parse(nome.Substring(nome.Length - 1));
+        blockPosion[0] = lin;
+        blockPosion[1] = col;
+    }
+
+    void checkPlayer()
+    {
+        if (!fall)
+        {
+            if (hasPlayer && !isSolidBlock)
+            {
+                fall = true;
+                gameObject.GetComponent<Rigidbody>().useGravity = true;
+            }
+        }
+    }
+
+    void checkPositionToDestroy()
+    {
+        if (fall)
+        {
+            if (gameObject.GetComponent<Transform>().position.y < -30)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    IEnumerator timer(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+    }
+
+}
---
+++ Maze - LD 43/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public int speed;
+    private bool canMove;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void checkMovement()
+    {
+        if (canMove)
+        {
+            if (Input.GetButtonDown("up"))
+            {
+                canMove = false;
+                Vector3 target = new Vector3(transform.position.x, transform.position.y + 1);
+                movePlayer(target);
+            }
+            if (Input.GetButtonDown("down"))
+            {
+                canMove = false;
+                Vector3 target = new Vector3(transform.position.x, transform.position.y - 1);
+                movePlayer(target);
+            }
+            if (Input.GetButtonDown("right"))
+            {
+                canMove = false;
+                Vector3 target = new Vector3(transform.position.x + 1, transform.position.y);
+                movePlayer(target);
+            }
+            if (Input.GetButtonDown("left"))
+            {
+                canMove = false;
+                Vector3 target = new Vector3(transform.position.x - 1, transform.position.y);
+                movePlayer(target);
+            }
+        }
+    }
+
+    void movePlayer(Vector3 target)
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
+    }
+}
